85e91bb249ec44fedbfe1da8dae50d2430afd628
wey1998 <yitwey1998@hotmail.com>
2023-04-09 01:14:16 +0100

    Add functionality to the boxes when clicked

--- script/index.js
+++ script/index.js
@@ -1,15 +1,31 @@
-let gamePattern = []
+const gamePattern = [];
+const userClickedPattern = [];
 const buttonColors = ["green", "blue", "red", "yellow"];
 
+function playSound(color) {
+  const audio = new Audio("sounds/" + color + ".mp3");
+  audio.play();
+}
+
 function nextSequence() {
-    const randomNumber = Math.floor( Math.random() * 4);
-    const randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor);
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
 
-    $("."+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    const audio = new Audio("sounds/"+randomChosenColor+".mp3");
-    audio.play();
-} 
+  $("." + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
 
+  playSound(randomChosenColor);
+}
+$(".btn").on("click", function (event) {
+  let userChosenColour = event.target.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userChosenColour);
+
+  playSound(userChosenColour);
+});
 
 nextSequence();
